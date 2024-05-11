c7af0e44a91de6c270df6d079f72b5a78606e27b
ㅤlucasㅤ <64121741+heyylucazzz@users.noreply.github.com>
2024-05-11 17:33:37 -0300

    KVM: make kvm socket expose in devfs

--- arch/arm64/include/asm/virt.h
+++ arch/arm64/include/asm/virt.h
@@ -81,17 +81,19 @@ static inline bool is_pkvm_initialized(void)
 /* Reports the availability of HYP mode */
 static inline bool is_hyp_mode_available(void)
 {
-	/*
-	 * If KVM protected mode is initialized, all CPUs must have been booted
-	 * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
-	 */
-	if (is_pkvm_initialized())
-		return true;
+    /*
+     * If KVM protected mode is initialized, all CPUs must have been booted
+     * in EL2. Avoid checking __boot_cpu_mode as CPUs now come up in EL1.
+     */
+    if (IS_ENABLED(CONFIG_KVM) &&
+        static_branch_likely(&kvm_protected_mode_initialized))
+        return true;
 
-	return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
-		__boot_cpu_mode[1] == BOOT_CPU_MODE_EL2);
+    /*
+    return (__boot_cpu_mode[0] == BOOT_CPU_MODE_EL2 &&
+        __boot_cpu_mode[1] == BOOT_CPU_MODE_EL2);*/
+    return 1;
 }
-
 /* Check if the bootloader has booted CPUs in different modes */
 static inline bool is_hyp_mode_mismatched(void)
 {
